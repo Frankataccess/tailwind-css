914b00aabad093a08b60bfcd8b317d8d9b543e16
Frankataccess <frank.mccann.355@acccesscreative.ac.uk>
2024-10-07 15:44:18 +0100

    changes

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,31 +1,42 @@
 import BlogPreview from "./components/BlogPreview";
 import NavBar from "./components/navbar";
+import { useState, useEffect } from "react";
 
-const  App= () => {
-  
-  //Logic
+const App = () => {
+  // useState
+  const [title, setTitle] = useState("Hello");
 
-  const title = "Hello"
+  const [lessons, setLessons] = useState([
+    { title: "Intro to React", author: "Joel", id: 1 },
+    { title: "Front end dev", author: "Joel", id: 2 },
+    { title: "Figma Tutorial", author: "Joel", id: 3 },
+    { title: "Flask", author: "Joe", id: 4 },
+    { title: "Industry Concepts", author: "Brad", id: 5 },
+  ]);
 
-  return ( 
+  const handleDelete = (id) => {
+    const newLessons = lessons.filter((l) => l.id !== id);
+    setLessons(newLessons);
+  };
+
+  useEffect(() => {
+    console.log("refreshed");
+  }, []);
+
+  return (
     <div className="bg-slate-500">
       <NavBar />
-      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, magni!</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, quos.</p>
       <div className="content">
-        <a href="https://vitejs.dev/guide/" target="_blank">Learn React</a>
-        <BlogPreview/>
-        <BlogPreview/>
-        <BlogPreview/>
-        <BlogPreview/>
-        <BlogPreview/>
-        <BlogPreview/>
-        <BlogPreview/>
+        <h1 className="text-5xl">{title}</h1>
 
+        <button onClick={() => setTitle("Bonjour")}>Change Title</button>
+
+        {lessons.map((l) => (
+          <BlogPreview lesson={l} key={l.id} handleDelete={handleDelete} />
+        ))}
       </div>
     </div>
   );
-}
-
+};
 
 export default App;
--- blog/src/components/BlogPreview.jsx
+++ blog/src/components/BlogPreview.jsx
@@ -1,13 +1,13 @@
-const BlogPreview = () => {
-    return ( 
-        <>
-        <section>
-        <h2 className="Section-Title">
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, quam doloribus debitis quas minima in eum dignissimos voluptates quo dicta, praesentium culpa rem ad iusto eius perferendis, molestiae quia ipsam dolorem temporibus! Laboriosam, esse ullam. Adipisci mollitia non possimus molestias officiis odio porro. Perspiciatis, perferendis autem corporis accusantium, eligendi provident voluptatum, veniam soluta distinctio dolor explicabo iure. Fugit laudantium, nostrum sunt ab nulla quibusdam quae iusto nisi reiciendis tenetur nesciunt fuga pariatur, illum, omnis consectetur? Eveniet necessitatibus in mollitia obcaecati autem dicta nemo expedita commodi harum debitis molestias provident, laudantium neque distinctio. Nobis architecto sapiente libero reiciendis veniam voluptate repellendus!</p>
-        </h2>
-        </section>
-        </>
+const BlogPreview = ({ lesson, handleDelete }) => {
+    return (
+    <section>
+        <h2>{lesson.title}</h2>
+        <p>{lesson.author}</p>
+        <button className="bg-red-400 text-white" onClick={() => handleDelete(lesson.id)}>
+        Delete Lesson
+        </button>
+    </section>
     );
-}
+};
 
 export default BlogPreview;
