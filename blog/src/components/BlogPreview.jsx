09319c6d5689f1d15a1d0bb7b706c1ad093d3a66
Frankataccess <frank.mccann.355@acccesscreative.ac.uk>
2024-10-07 09:21:11 +0100

    initial commit

---
+++ blog/src/components/BlogPreview.jsx
@@ -0,0 +1,13 @@
+const BlogPreview = () => {
+    return ( 
+        <>
+        <section>
+        <h2 className="Section-Title">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, quam doloribus debitis quas minima in eum dignissimos voluptates quo dicta, praesentium culpa rem ad iusto eius perferendis, molestiae quia ipsam dolorem temporibus! Laboriosam, esse ullam. Adipisci mollitia non possimus molestias officiis odio porro. Perspiciatis, perferendis autem corporis accusantium, eligendi provident voluptatum, veniam soluta distinctio dolor explicabo iure. Fugit laudantium, nostrum sunt ab nulla quibusdam quae iusto nisi reiciendis tenetur nesciunt fuga pariatur, illum, omnis consectetur? Eveniet necessitatibus in mollitia obcaecati autem dicta nemo expedita commodi harum debitis molestias provident, laudantium neque distinctio. Nobis architecto sapiente libero reiciendis veniam voluptate repellendus!</p>
+        </h2>
+        </section>
+        </>
+    );
+}
+
+export default BlogPreview;
---
+++ blog/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import './navbar.css'
+
+const NavBar = () => {
+
+    const title = "hello"
+
+    return (
+        <header className="app-header">
+            <h1>{title}</h1>
+        </header>
+    );
+}
+
+export default NavBar;
